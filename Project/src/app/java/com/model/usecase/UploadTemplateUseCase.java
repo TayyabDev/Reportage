4b49c84b470c4d6932a164c83bc573ed66edd381
linjun9 <junlinjun29@gmail.com>
2018-11-02 22:37:28 -0400

    add the InsertCommands in the UploadTemplateUseCase

--- Project/src/app/java/com/model/usecase/UploadTemplateUseCase.java
+++ Project/src/app/java/com/model/usecase/UploadTemplateUseCase.java
@@ -1,23 +1,28 @@
 package app.java.com.model.usecase;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.java.com.model.database.api.Command;
+import app.java.com.model.database.api.InsertCommand;
 import app.java.com.model.utilities.ExcelFile;
 import app.java.com.presenter.interfaces.UploadTemplateResultInterface;
 
 public class UploadTemplateUseCase extends UseCase {
 
-    private String templateFileName;
+    private String templateFilePath;
     private UploadTemplateResultInterface resultInterface;
     private static final int SHEET_NUMBER = 0;
 
-    public UploadTemplateUseCase(UploadTemplateResultInterface resultInterface, String templateFileName) {
+    public UploadTemplateUseCase(UploadTemplateResultInterface resultInterface, String templateFilePath) {
         this.resultInterface = resultInterface;
-        this.templateFileName = templateFileName;
+        this.templateFilePath = templateFilePath;
     }
 
     @Override
     public void run() {
 
-        String formulatedFileName = templateFileName.replace("\\", "\\\\");
+        String formulatedFileName = templateFilePath.replace("\\", "\\\\");
         System.out.println(formulatedFileName);
         ExcelFile exc = new ExcelFile(formulatedFileName);
 
@@ -27,7 +32,20 @@ public class UploadTemplateUseCase extends UseCase {
 
 
         // Upload the template into the database using insert command
-
+        List<String> columnIds = null;
+        List<String> row = null;
+        String templateName = null;
+        int numOfRow = 0;
+        List<Integer> errorList = new ArrayList<Integer>();
+        int i;
+        for (i = 3; i < numOfRow; i++){
+        	Command insert = new InsertCommand(templateName,columnIds, row);
+        	try {
+				insert.handle();
+			} catch (Exception e) {
+				errorList.add(i);
+			}
+        }
         // Notify the presenter
 
 
