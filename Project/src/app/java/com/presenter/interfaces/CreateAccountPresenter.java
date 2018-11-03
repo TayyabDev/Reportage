de042d20f3e9c86425802daf169451b484c0c069
Tayyab <waqartay@gmail.com>
2018-11-03 16:08:35 -0400

    Implemented the presenter methods and View methods for Account

--- Project/src/app/java/com/presenter/interfaces/CreateAccountPresenter.java
+++ Project/src/app/java/com/presenter/interfaces/CreateAccountPresenter.java
@@ -1,11 +1,11 @@
 package app.java.com.presenter.interfaces;
 
-import app.java.com.view.interfaces.CreateTemplateView;
+import app.java.com.view.interfaces.CreateAccountView;
 
 public interface CreateAccountPresenter {
     void createAccount(String name, String password);
 
-    void attachView(CreateTemplateView view);
+    void attachView(CreateAccountView view);
 
     void unbindView();
 }
--- Project/src/app/java/com/view/ui/createAccountViews/Account.java
+++ Project/src/app/java/com/view/ui/createAccountViews/Account.java
@@ -1,5 +1,8 @@
 package app.java.com.view.ui.createAccountViews;
 
+import app.java.com.presenter.CreateAccountPresenterImpl;
+import app.java.com.presenter.interfaces.CreateAccountPresenter;
+import app.java.com.view.interfaces.CreateAccountView;
 import app.java.com.view.ui.UIHelpers;
 
 import java.awt.*;
@@ -9,20 +12,26 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class Account {
+public class Account implements CreateAccountView {
 	
 	 private JPanel panel;
 	 private JButton create;
 	 private JButton edit;
 	 private JButton search;
 	 private static JFrame frame;
+	 private CreateAccountPresenter presenter;
 
      public Account() {
+
+
     	 GridBagLayout gb = new GridBagLayout();
  		GridBagConstraints c = new GridBagConstraints();
  		panel = new JPanel();
  		panel.setLayout(gb);
  		panel.setBackground(Color.decode("#f1f8e9"));
+
+ 		presenter = new CreateAccountPresenterImpl();
+ 		presenter.attachView(this);
  		
  		
  		c.fill = GridBagConstraints.BOTH;
@@ -32,8 +41,7 @@ public class Account {
     	 create = UIHelpers.buttonGenerator("Create an Account");
     	 
     	 // create account dialog box
-    	 create.addActionListener(new ActionListener()
-    	    {
+    	 create.addActionListener(new ActionListener() {
     	      public void actionPerformed(ActionEvent e)
     	      {
     	        // create a new form to accept user input
@@ -44,22 +52,16 @@ public class Account {
 
 				  JLabel lblEnterInfo = new JLabel("Please specify the name and password.");
 				  lblEnterInfo.setBounds(150,5, 400, 20);
-
 				  JLabel lblName = new JLabel("Name");
 				  lblName.setBounds(150, 50, 400, 20);
-
 				  JTextField txtName = new JTextField();
 				  txtName.setBounds(150, 100, 400, 20);
-
 				  JLabel lblPassword = new JLabel("Password");
 				  lblPassword.setBounds(150, 150, 400, 20);
-
 				  JPasswordField txtPassword = new JPasswordField();
 				  txtPassword.setBounds(150, 200, 400, 20);
-
 				  JButton submit = UIHelpers.buttonGenerator("Submit");
 				  submit.setBounds( 150, 250, 150,50);
-
 				  JButton cancel = UIHelpers.buttonGenerator("Cancel");
 				  cancel.setBounds(350, 250, 150, 50);
 
@@ -80,12 +82,14 @@ public class Account {
                       public void actionPerformed(ActionEvent e) {
                           //  NOTE TO PRESENTER: THIS IS THE USERS INFO!
                             String name = txtName.getText();
-                            char [] password = txtPassword.getPassword();
+                            String password = String.valueOf(txtPassword.getPassword());
 
                             // createAccount
                             // get account ID
                             // then show user dialog giving account id
-                            JOptionPane.showMessageDialog(frame, "Account created with ID <insert id>");
+						  	presenter.createAccount(name, password);
+
+
 
                           frame.setContentPane(panel);
                           frame.revalidate();
@@ -138,4 +142,37 @@ public class Account {
 
 	 }
 
+	@Override
+	public void onSuccessAccountCreated() {
+     	// must send in account id to user
+		JOptionPane.showMessageDialog(frame, "Account created with ID <insert id>.");
+	}
+
+	@Override
+	public void onErrorCreatingAccount() {
+		JOptionPane.showMessageDialog(frame, "Error creating the account.");
+	}
+
+	@Override
+	public boolean isFieldsValid(String name, String password) {
+     	System.out.println(name);
+		System.out.println(password);
+
+		boolean valid = true;
+		if (name.matches(".*\\d+.*")){
+			valid = false;
+		}
+		if(password.length() == 0){
+			valid = false;
+		}
+		System.out.println(valid);
+
+
+		return valid;
+	}
+
+	@Override
+	public void invalidFields() {
+		JOptionPane.showMessageDialog(frame, "Error. Please make sure names do not contain any numbers and passwords are greater than length 0.");
+	}
 }
