01e4cd4014503a32d7ae011a80bfcdcb7ed3e8ad
Tayyab Waqar <tayyab.waqar@mail.utoronto.ca>
2018-11-05 12:25:11 -0500

    Fixed bug in SelectCommand where it wasnt accepting multiple constraints. Implemented Login for user which utilisese VerifyUseCase to let user log in to software.

---
+++ Project/src/app/java/com/presenter/LoginPresenterImpl.java
@@ -0,0 +1,49 @@
+package app.java.com.presenter;
+
+import app.java.com.model.usecase.VerifyAccountUseCase;
+import app.java.com.presenter.interfaces.LoginPresenter;
+import app.java.com.presenter.interfaces.LoginResultInterface;
+import app.java.com.view.interfaces.LoginView;
+
+public class LoginPresenterImpl implements LoginPresenter, LoginResultInterface {
+
+    private LoginView view;
+
+    public LoginPresenterImpl(LoginView view){
+        this.view = view;
+    }
+
+    @Override
+    public void onSuccessLogin() {
+        view.onSuccessLogin();
+
+    }
+
+    @Override
+    public void onErrorLogin() {
+        view.onErrorLogin();
+
+    }
+
+    @Override
+    public void verifyAccount(String username, String password) {
+        if(view.isFieldsValid(username, password)){
+            VerifyAccountUseCase useCase = new VerifyAccountUseCase(this, username,password);
+            useCase.run();
+        } else {
+            view.invalidFields();
+        }
+    }
+
+    @Override
+    public void attachView(LoginView view) {
+        this.view = view;
+
+    }
+
+    @Override
+    public void unbindView() {
+        this.view = null;
+
+    }
+}
