2f81a9981cf317c96c4595d2d8f9c34bd0fe45c3
linjun9 <junlinjun29@gmail.com>
2018-11-19 09:39:21 -0500

    refactor the AccountTypeFinder and UserTypeFinder classes

--- Project/src/app/java/com/model/entities/user/UserTypeFinder.java
+++ Project/src/app/java/com/model/entities/user/UserTypeFinder.java
@@ -5,10 +5,6 @@ import app.java.com.model.entities.Type;
 public class UserTypeFinder {
 
 	public static boolean isTeqStaff(User user) {
-		if (user.getUserType().compareTo(Type.TEQ.getType()) == 0) {
-			return true;
-		} else {
-			return false;
-		}
+		return (user.getUserType().compareTo(Type.TEQ.getType()) == 0);
 	}
 }
