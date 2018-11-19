fc3c46df9de6471066a6471ecad5f618e40a19ad
Tayyab Waqar <tayyab.waqar@mail.utoronto.ca>
2018-11-19 11:39:34 -0500

    Added a raw layout for the user interface for resolving conflicts. Scrollbar implemented with conflicts.

--- Project/src/app/java/com/view/ui/uploadTemplateViews/ResolveConflicts.java
+++ Project/src/app/java/com/view/ui/uploadTemplateViews/ResolveConflicts.java
@@ -1,11 +1,15 @@
 package app.java.com.view.ui.uploadTemplateViews;
 
 import app.java.com.model.Exceptions.InsertException;
+import app.java.com.model.entities.account.TeqAccount;
 import app.java.com.view.interfaces.ResolveConflictsView;
 import app.java.com.view.ui.UIHelpers;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ResolveConflicts implements ResolveConflictsView {
@@ -20,18 +24,60 @@ public class ResolveConflicts implements ResolveConflictsView {
     private JPanel panel;
 
     private List<InsertException> errors;
+    private JScrollPane scrollPane;
+    private JPanel scrollPanel;
 
-    public ResolveConflicts(JFrame frame){
+
+    public ResolveConflicts(JFrame frame, TeqAccount account){
         this.frame = frame;
 
         panel = new JPanel();
         panel.setLayout(null);
 
-        JLabel lblConflicts = new JLabel("You have conflicts. Please fix them below.");
+
+        scrollPanel = new JPanel(new GridLayout(0, 1));
+        scrollPanel.setBorder(BorderFactory.createTitledBorder("Please resolve the conflicts."));
+
+        ButtonGroup bg = new ButtonGroup();
+        List<JRadioButton> errorOptionButtons = new ArrayList<>();
+        for (int i =0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                JRadioButton jrb = new JRadioButton(String.format("Conflict in Row %d Col %d", i,j));
+                bg.add(jrb);
+                errorOptionButtons.add(jrb);
+                scrollPanel.add(jrb);
+            }
+        }
 
 
 
 
+        scrollPane = new JScrollPane(scrollPanel);
+        scrollPane.setBounds(50,0,900,450);
+
+
+
+
+        JButton resolve = UIHelpers.buttonGenerator("Resolve");
+        resolve.setBounds(450, 470, 100,20);
+        resolve.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for(JRadioButton errorOption : errorOptionButtons){
+                    if(errorOption.isSelected()){
+                        // conflict
+                        String [] options = {"Use old", "Use new"};
+
+                        // invalid
+                    }
+                }
+            }
+        });
+
+
+
+        panel.add(scrollPane);
+        panel.add(resolve);
 
         this.frame.setContentPane(panel);
         this.frame.revalidate();
@@ -48,7 +94,7 @@ public class ResolveConflicts implements ResolveConflictsView {
         frame.setResizable(false);
         UIHelpers.setLook();
 
-        ResolveConflicts c = new ResolveConflicts(frame);
+        ResolveConflicts c = new ResolveConflicts(frame, null);
     }
 
     @Override
